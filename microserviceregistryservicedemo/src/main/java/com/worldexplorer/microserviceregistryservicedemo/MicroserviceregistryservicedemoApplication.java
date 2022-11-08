99a0aea2c0ce5ae3b28ff99b2a8c4aa5555d1a97
manolopzy <manolopeng@gmail.com>
2022-11-08 21:37:18 +0100

    spring cloud microservice registration server service using eureka server

---
+++ microserviceregistryservicedemo/src/main/java/com/worldexplorer/microserviceregistryservicedemo/MicroserviceregistryservicedemoApplication.java
@@ -0,0 +1,14 @@
+package com.worldexplorer.microserviceregistryservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroserviceregistryservicedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceregistryservicedemoApplication.class, args);
+	}
+
+}
---
+++ microserviceregistryservicedemo/src/test/java/com/worldexplorer/microserviceregistryservicedemo/MicroserviceregistryservicedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldexplorer.microserviceregistryservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceregistryservicedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
