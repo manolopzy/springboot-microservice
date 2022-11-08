5d186f430b8b083d31e8333c26da872fe6f0ca0e
manolopzy <manolopeng@gmail.com>
2022-11-08 21:33:51 +0100

    Spring cloud configuration server demo using git as configuration file center

---
+++ configserverdemo/src/main/java/com/worldexplorer/configserverdemo/ConfigserverdemoApplication.java
@@ -0,0 +1,14 @@
+package com.worldexplorer.configserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigserverdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverdemoApplication.class, args);
+	}
+
+}
---
+++ configserverdemo/src/test/java/com/worldexplorer/configserverdemo/ConfigserverdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldexplorer.configserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
